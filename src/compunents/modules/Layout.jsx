45f673d54248b85f12bebeae685f3366b2cea639
Alireza Nayyeri <dev.alirezanayyeri@gmail.com>
2025-03-21 01:45:54 +0330

    add footer and css footer

--- src/compunents/modules/Layout.jsx
+++ src/compunents/modules/Layout.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import styles from "./Layout.module.css"
 function Layout({children}) {
   return (
-    <div>
+    <div className={styles.container}>
         <header className={styles.header}>
             <h1>Contact Project</h1>
             <p><span>Ali-Star</span> | React.js </p>
         </header>
           {children}
-        <footer></footer>
+        <footer className={styles.footer}>
+            <h1>Developed by Alireza Nayyeri</h1>
+        </footer>
     </div>
   )
 }
